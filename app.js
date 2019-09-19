3babbb1fc432184edee9d16ab96273548eb250e2
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2019-09-19 11:43:42 -0700

    madlibCreation

---
+++ app.js
@@ -0,0 +1,6 @@
+var libButton = document.getElementById('lib-button');
+var libIt = function() {
+    var storyDiv = document.getElementById("story");
+    storyDiv.innerHTML = "??Your story here";
+};
+libButton.addEventListener('click', libIt);
